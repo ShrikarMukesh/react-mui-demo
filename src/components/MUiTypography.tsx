b4ffd0476c5ae6f0f70f1a5e813b4977d55edceb
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-03-26 23:11:09 +0530

    MUi Button

---
+++ src/components/MUiTypography.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export const MUiTypography = () => {
+  return (
+    <div>
+      {" "}
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="subtitle1">subtitle1</Typography>
+      <Typography variant="subtitle1">subtitle1</Typography>
+      <Typography variant="body1">
+        I really appreciate for your content. I already go through all of your
+        React JS Tutorial playlist, I am looking for MUI with Emotion library,
+        finally again I came back to your Channel. One small suggestion, Could
+        you please change your VS CODE Theme as same as before what you used in
+        React JS tutorial. That is more convenient some times when I watch it on
+        Mobile. To be honest with you. I don't see any other better channel for
+        React. Thanks once again
+      </Typography>
+      <Typography variant="body2">
+        I really appreciate for your content. I already go through all of your
+        React JS Tutorial playlist, I am looking for MUI with Emotion library,
+        finally again I came back to your Channel. One small suggestion, Could
+        you please change your VS CODE Theme as same as before what you used in
+        React JS tutorial. That is more convenient some times when I watch it on
+        Mobile. To be honest with you. I don't see any other better channel for
+        React. Thanks once again
+      </Typography>
+    </div>
+  );
+};
